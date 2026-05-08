9d79a9a8e4dc3a891d2010006e1f7c04721d7f2e
kensukesuzuki04 <kensuke.suzuki04@gmail.com>
2026-05-08 11:24:55 -0400

    change deploy id

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Set your deployed Google Apps Script URL here after deployment.
 // Deploy > New deployment > Web app > Execute as: Me > Access: Anyone
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzFxeLbLG6LOPh36XWMoSLYmZpw7qDLbI--Y_UFZ9Rgr8r287JNXZC0nk8Z43svP_74KQ/exec',
   BASE_PATH: '/dashboard'   // matches GitHub Pages subfolder
 };
